9e186dd55b868739c5f4b8afd025b8ccfd74c081
The Woops <globosch@web.de>
2019-02-20 17:46:27 +0100

    Fixed IndexError: list index out of range

--- kununu_project/spiders/kununu.py
+++ kununu_project/spiders/kununu.py
@@ -8,15 +8,23 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         self.log('I just visited: ' + response.url)
-        for elem in response.css('article.company-profile-review'):
+        review_list = response.css('article.company-profile-review')
+        for elem in review_list:
                     date = {
                                'date': elem.css('span::text')[1].extract(),
                                'title': elem.css('a::text')[0].extract(),
                                'rating': elem.css('div.tile-heading::text')[0].extract()
                             }
                     yield date
-        next_page_url = response.css('a.btn.btn-default.btn-block::attr(href)')[0].extract()
+
+        next_page_url = response.css('a.btn.btn-default.btn-block::attr(href)') # does this attribute exist at all or is returned an empty list?
         if next_page_url:
+           next_page_url = next_page_url[0].extract()
            print(" Aktuelle Seite" + next_page_url)
            next_page_url = response.urljoin(next_page_url)
            yield scrapy.Request(url=next_page_url, callback = self.parse)
+        else:
+            self.log('')
+            self.log('Last page reached:' + response.url)
+            self.log('Last page contained {} item(s)'.format(len(review_list)))
+            self.log('')
