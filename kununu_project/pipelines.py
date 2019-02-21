bfad1728d7ef3b91daa83aecac7bf6b1d7186dee
The Woops <globosch@web.de>
2019-02-21 10:26:43 +0100

    Added item date replacment to pipline

--- kununu_project/pipelines.py
+++ kununu_project/pipelines.py
@@ -12,4 +12,19 @@ class FirstPipeline(object):
         item['rating'] = item['rating'].strip()
         item['rating'] = float(item['rating'].replace(',', "."))
         print("pipeline: {} ".format(item['rating']))
+
+        item['date'] = item['date'].replace('Jan. ', "01.")
+        item['date'] = item['date'].replace('Feb. ', "02.")
+        item['date'] = item['date'].replace('März', "03.")
+        item['date'] = item['date'].replace('Apr. ', "04.")
+        item['date'] = item['date'].replace('Mai ', "05.")
+        item['date'] = item['date'].replace('Juni ', "06.")
+        item['date'] = item['date'].replace('Juli ', "07.")
+        item['date'] = item['date'].replace('Aug. ', "08.")
+        item['date'] = item['date'].replace('Sep. ', "09.")
+        item['date'] = item['date'].replace('Okt. ', "10.")
+        item['date'] = item['date'].replace('Nov. ', "11.")
+        item['date'] = item['date'].replace('Dez. ', "12.")
+        item['date'] = item['date'].replace(' ', "")
+        
         return item
